7ae08059f0949ef1645fee6050e85dcc272300ee
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-01-10 16:49:53 +0200

    Update ApiController.java

--- Java/Apache Wicket /ApiController.java
+++ Java/Apache Wicket /ApiController.java
@@ -1,10 +1,8 @@
-// HelloApiController.java
 package com.example;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.request.resource.IResource;
 import org.apache.wicket.request.resource.StringResource;
 
 public class HelloApiController extends WebPage {
@@ -36,18 +34,7 @@ public class HelloApiController extends WebPage {
     }
 
     public static void main(String[] args) {
-        org.apache.wicket.protocol.http.WicketServlet wicketServlet = new org.apache.wicket.protocol.http.WicketServlet();
-        org.eclipse.jetty.server.Server server = new org.eclipse.jetty.server.Server(8080);
-        org.eclipse.jetty.servlet.ServletContextHandler contextHandler = new org.eclipse.jetty.servlet.ServletContextHandler();
-        contextHandler.addServlet(new org.eclipse.jetty.servlet.ServletHolder(wicketServlet), "/*");
-        contextHandler.setInitParameter("applicationClassName", HelloApiApplication.class.getName());
-        server.setHandler(contextHandler);
-
-        try {
-            server.start();
-            server.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Start the Wicket application using its built-in server
+        new HelloApiApplication().run();
     }
 }
