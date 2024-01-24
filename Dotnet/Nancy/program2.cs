2b194d9e730fc3551de0d728849f9f985cfc29a8
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-01-24 15:51:10 +0200

    Create program2.cs

---
+++ Dotnet/Nancy/program2.cs
@@ -0,0 +1,30 @@
+using System;
+using Nancy.Hosting.Self;
+
+
+class Program
+{
+    private string _url = "http://localhost";
+    private int _port = 12345;
+    private NancyHost _nancy;
+
+    public Program()
+    {
+        var uri = new Uri( $"{_url}:{_port}/");
+        _nancy = new NancyHost(uri);
+    }
+
+    private void Start()
+    {
+        _nancy.Start();
+        Console.WriteLine($"Started listennig port {_port}");
+        Console.ReadKey();
+        _nancy.Stop();
+    }
+
+    static void Main(string[] args)
+    {
+        var p = new Program();
+        p.Start();
+    }
+}
